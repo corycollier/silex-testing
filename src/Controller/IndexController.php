fff2203fa0713b57ab0b59f61e71c9f527e64a56
Cory Collier <cory.collier@ahss.org>
2016-07-18 17:06:43 -0400

    adding a bunch of stuff

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+class IndexController
+{
+}
---
+++ src/Form/Type/ImageType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+class ImageType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('name', 'text', [
+				'constraints' => new Assert\NotBlank,
+			])
+			->add('save', 'submit');
+	}
+}
